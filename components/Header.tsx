084d95beb63def900cc4b294deaafbf565961f34
Mahdi Parivash <mahdiparivsh@protonmail.com>
2022-09-27 23:29:14 +0330

    add assets and update header and hero section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   const router = useRouter();
+  const [show, setshow] = useState(false);
 
   const navbar = [
     {
@@ -24,13 +26,10 @@ function Header() {
     },
 
     {
-      name: "جاذبه های گردشگری ایران",
-      path: "/touriran",
+      name: "جاذبه های گردشگری ",
+      path: "/tourism",
     },
-    {
-      name: "جاذبه های گردشگری آلمان",
-      path: "/tourgermany",
-    },
+
     {
       name: "آخرین اخبار",
       path: "/news",
@@ -38,27 +37,51 @@ function Header() {
   ];
 
   return (
-    <div
-      className="flex font-semibold sticky z-50 bg-[#f6f7fe] top-0 text-base w-full justify-around items-center mt-2 h-20 shadow-3xl
-"
-    >
-      <button className="text-white px-4 py-2 bg-primary rounded-lg">
-        <Link href="signup">ورود/ثبت نام</Link>
-      </button>
-      <div className="flex justify-between  items-center  gap-8">
-        {navbar.map((item) => (
-          <Link href={item.path} key={item.name}>
-            <a
-              className={
-                router.pathname === item.path ? "text-landing-50" : "text-black"
-              }
-            >
-              {item.name}
-            </a>
-          </Link>
-        ))}
-      </div>
-      <p>Logo</p>
+    <div>
+      <nav
+        className="flex font-semibold sticky z-40 bg-[#f6f7fe] top-0 text-base w-full justify-between lg:justify-around items-center mt-2 h-20 lg:shadow-3xl
+      "
+      >
+        <button className="text-white hidden xl:flex px-4 py-2 bg-primary rounded-lg">
+          <Link href="signup">ورود/ثبت نام</Link>
+        </button>
+        <div className="lg:hidden cursor-pointer">
+          {show ? (
+            <XMarkIcon
+              onClick={() => setshow(false)}
+              className="inline xl:hidden w-6 h-6 mr-4"
+            />
+          ) : (
+            <Bars3Icon
+              onClick={() => setshow(true)}
+              className="inline xl:hidden w-6 h-6 mr-4"
+            />
+          )}
+        </div>
+
+        <div
+          className={`${
+            show
+              ? "flex flex-col relative left-20 bg-[#f6f7fe]  md:left-44 p-2 text-right top-52 w-1/2 z-50 transition-all ease-out"
+              : "hidden"
+          }  lg:flex justify-between md:file:items-center  gap-8`}
+        >
+          {navbar.map((item) => (
+            <Link href={item.path} key={item.name}>
+              <a
+                className={
+                  router.pathname === item.path
+                    ? "text-landing-50"
+                    : "text-black"
+                }
+              >
+                {item.name}
+              </a>
+            </Link>
+          ))}
+        </div>
+        <p className="ml-4 lg:ml-0">Logo</p>
+      </nav>
     </div>
   );
 }
--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import herophoto from "../assets/Rectangle 3.png";
+import herophoto from "../../assets/Rectangle 3.png";
+
 function HeroSection() {
   return (
-    <div className=" flex justify-around items-center my-6">
-      <div className="flex-1 mr-24 ">
+    <div className=" flex flex-col justify-center sm:flex-row sm:justify-around items-center my-6">
+      <div className="mr-4 xl:mr-24 ">
         <Image
           alt="herosection"
           height={"538px"}
@@ -13,12 +14,12 @@ function HeroSection() {
           src={herophoto}
         />
       </div>
-      <div className="flex flex-col flex-1">
-        <p className="font-bold  text-5xl leading-snug w-full">
+      <div className="flex flex-col">
+        <p className="font-bold text-2xl  lg:text-5xl leading-snug w-full">
           زندگی‌ای را بکن که <br />
           همیشه آرزوی آن را داشته ای
         </p>
-        <p className="font-normal text-2xl my-6">
+        <p className="font-normal text-lg lg:text-2xl my-6">
           زندگی کوتاه است و دنیا بسیار بزرگ بنابراین <br /> همین الان شروع کن!
         </p>
         <button className="text-white p-4 w-52 bg-primary rounded-lg">
